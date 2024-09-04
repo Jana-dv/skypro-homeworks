248dd93a82f78b822d41f8544a9bd794c0636aea
Jana-dv <yana.devrisheva@gmail.com>
2024-09-04 16:13:21 +0300

    Добавили файлы с домашним заданием.

---
+++ lesson_1_task_7.py
@@ -0,0 +1,42 @@
+def num1():
+    n1 = 1
+    print(n1, end='')
+def num2():
+    n2 = 2
+    print(n2, end='')
+def num3():
+    n3 = 3
+    print(n3, end='')
+def num4():
+    n4 = 4
+    print(n4, end='')
+def num5():
+    n5 = 5
+    print(n5, end='')
+def num6():
+    n6 = 6
+    print(n6, end='')
+def num7():
+    n7 = 7
+    print(n7, end='')
+def num8():
+    n8 = 8
+    print(n8, end='')
+def num9():
+    n9 = 9
+    print(n9, end='')
+def num0():
+    n0 = 0
+    print(n0, end='')
+
+num8()
+num8()
+num0()
+num0()
+num5()
+num5()
+num5()
+num3()
+num5()
+num3()
+num5()
